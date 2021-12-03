709b98c133e04a2dec1df23c08f009d924bb178a
houseofsam <samxsaid@gmail.com>
2021-12-03 05:20:15 +0000

    completed api call 1 for ip address

---
+++ index.js
@@ -0,0 +1,10 @@
+// const { fetchMyIP } = require('./iss');
+
+// fetchMyIP((err, ip) => {
+//   if (err) {
+//     console.log('It didn\'t work!', error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:', ip);
+// });
